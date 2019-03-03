bc245a7dbe886e13883c274a4d58602c4a8f2d62
Franz Xaver Schmid <42220771+franzxschmid@users.noreply.github.com>
2019-03-03 23:33:12 +0100

    Add files via upload

---
+++ my_library.py
@@ -0,0 +1,92 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+
+def frequency_encoding(df, variable):
+    t = df[variable].value_counts().reset_index()
+    t = t.reset_index()
+    t.loc[t[variable] == 1, 'level_0'] = np.nan
+    t.set_index('index', inplace=True)
+    max_label = t['level_0'].max() + 1
+    t.fillna(max_label, inplace=True)
+    return t.to_dict()['level_0']
+
+def missing_data(data):
+    total = data.isnull().sum()
+    percent = (data.isnull().sum()/data.isnull().count()*100)
+    tt = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    types = []
+    for col in data.columns:
+        dtype = str(data[col].dtype)
+        types.append(dtype)
+    tt['Types'] = types
+    return(np.transpose(tt))
+
+def plot_feature_distribution(df1, df2, label1, label2, features):
+    i = 0
+    sns.set_style('whitegrid')
+    plt.figure()
+    fig, ax = plt.subplots(10,10,figsize=(18,22))
+
+    for feature in features:
+        i += 1
+        plt.subplot(10,10,i)
+        sns.kdeplot(df1[feature], bw=0.5,label=label1)
+        sns.kdeplot(df2[feature], bw=0.5,label=label2)
+        plt.xlabel(feature, fontsize=9)
+        locs, labels = plt.xticks()
+        plt.tick_params(axis='x', which='major', labelsize=6, pad=-6)
+        plt.tick_params(axis='y', which='major', labelsize=6)
+    plt.show();
+    
+    
+def plot_new_feature_distribution(df1, df2, label1, label2, features):
+    i = 0
+    sns.set_style('whitegrid')
+    plt.figure()
+    fig, ax = plt.subplots(1,9,figsize=(18,4))
+
+    for feature in features:
+        i += 1
+        plt.subplot(1,9,i)
+        sns.kdeplot(df1[feature], bw=0.5,label=label1)
+        sns.kdeplot(df2[feature], bw=0.5,label=label2)
+        plt.xlabel(feature, fontsize=9)
+        locs, labels = plt.xticks()
+        plt.tick_params(axis='x', which='major', labelsize=6, pad=-6)
+        plt.tick_params(axis='y', which='major', labelsize=6)
+    plt.show();
+
+
+
+
